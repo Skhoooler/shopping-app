80c8291565303b1c80e0e01d665359742b494853
Skhoooler <dor15004@byui.edu>
2020-06-03 12:18:23 -0600

    changed the thing

--- Shopface/app/src/main/java/com/doria/byui/shopface/MainActivity.java
+++ Shopface/app/src/main/java/com/doria/byui/shopface/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);///this is the main activity
+        setContentView(R.layout.activity_main);///tHiS iS tHe MaIn AcTiViTy
         // No ur wrong, THIS is the main activity
     }
 }
