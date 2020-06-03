691e2190a36ad3cea1547e687bcc0492e4562a43
Skhoooler <dor15004@byui.edu>
2020-06-03 12:12:43 -0600

    Added a comment

--- Shopface/app/src/main/java/com/doria/byui/shopface/MainActivity.java
+++ Shopface/app/src/main/java/com/doria/byui/shopface/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);///this is the main activity
+        // No ur wrong, THIS is the main activity
     }
 }
