49d2911778d2e893b16cf36be990c849eff3e318
Skhoooler <dor15004@byui.edu>
2020-06-10 12:45:43 -0600

    Added some Unit tests for Search

---
+++ Shopface/app/src/main/java/com/doria/byui/shopface/Search.java
@@ -0,0 +1,14 @@
+package com.doria.byui.shopface;
+
+import java.io.File;
+
+class Search {
+    private String query;
+    private File file;
+    Search(){
+
+    }
+    private void storeSearch(){
+
+    }
+}
--- Shopface/app/src/test/java/com/doria/byui/shopface/SearchTest.java
+++ Shopface/app/src/test/java/com/doria/byui/shopface/SearchTest.java
@@ -1,4 +1,25 @@
 package com.doria.byui.shopface;
 
+import org.junit.Test;
+import java.lang.reflect.Field;
+import static org.junit.Assert.*;
+
 public class SearchTest {
+
+    @Test
+    public void searchQueryTest() throws NoSuchFieldException {
+        Search tester = new Search();
+        Field query = Search.class.getDeclaredField("query");
+        query.setAccessible(true);
+
+        assertNotNull("Query String is null", query);
+    }
+
+    @Test
+    public void searchStoreSearchTest() throws NoSuchFieldException {
+        Field file = Search.class.getDeclaredField("file");
+        file.setAccessible(true);
+        assertNotNull("File object is null", file);
+    }
+
 }
