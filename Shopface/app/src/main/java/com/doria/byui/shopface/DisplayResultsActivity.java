914fd32c1a757dd6c85bb96dc12adcdd10308cd0
yugimumoto1 <sonnytoe96@gmail.com>
2020-06-12 12:24:51 -0600

    stubbed out display results and view

---
+++ Shopface/app/src/main/java/com/doria/byui/shopface/DisplayResultsActivity.java
@@ -0,0 +1,27 @@
+package com.doria.byui.shopface;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class DisplayResultsActivity extends AppCompatActivity {
+
+    public ShopFaceView displayProducts(String query);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_results);
+        super.onPause();
+    }
+    void onClickSearch(View view){
+
+    }
+    void onClickBack(View view){
+
+    }
+    void onClickProduct(View view){
+
+    }
+}
