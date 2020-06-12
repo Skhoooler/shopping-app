9cf2b71ea85af3c3ff34e0c490d462237c7d8127
Skhoooler <dor15004@byui.edu>
2020-06-12 12:33:51 -0600

    Finished stubbing stubbing out the Connect class

--- Shopface/app/src/main/java/com/doria/byui/shopface/Connect.java
+++ Shopface/app/src/main/java/com/doria/byui/shopface/Connect.java
@@ -1,4 +1,66 @@
 package com.doria.byui.shopface;
 
+import java.util.Map;
+
 class Connect {
+    // String is the name of the product
+    Map<String, Product> rawData;
+    String query;
+
+    Connect(String queryFromSearchBar){
+
+    }
+
+    Connect() {
+
+    }
+
+    /**
+     * Returns a map full of Product objects
+     * @return a map with the raw data from the connect interface
+     */
+    Map<String, Product> getAllData(){
+
+        return null;
+    }
+
+    /**
+     * Queries the different APIs to get the top 10 results from each website and puts
+     * it in a map
+     */
+    void search(){
+
+    }
+
+    /**
+     * Constructs URLS for all of the different website's who's data base is being queried
+     * @return a complete URL in string form
+     */
+    String constructURLS(){
+        return null;
+    }
+
+    /**
+     * Actually sends the query and receives the information from the database
+     * @return a string with the raw XML or JSON information from the database
+     */
+    String sendQuery(){
+        return null;
+    }
+
+    /**
+     * Deserializes the information from JSON to a Product object
+     * @return a Product object with the information stored in it
+     */
+    Product deserializeJSON(){
+        return null;
+    }
+
+    /**
+     * Deserializes the information from XML to a Product object
+     * @return a Product with the information stored in it
+     */
+    Product deserializeXML(){
+        return null;
+    }
 }
--- Shopface/app/src/main/java/com/doria/byui/shopface/ShopFaceModel.java
+++ Shopface/app/src/main/java/com/doria/byui/shopface/ShopFaceModel.java
@@ -6,16 +6,10 @@ class ShopFaceModel extends Connect{
     Map<String, Product> data;
 
     ShopFaceModel(String query){
+        super();
 
     }
 
-    /**
-     * Returns a map full of Product objects
-     * @return a map with the raw data from the connect interface
-     */
-    public Map<String, Product> getAllData(){
-        return null;
-    }
 
     /**
      * Not sure what we were thinking with this one
