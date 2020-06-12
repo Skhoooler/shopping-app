bc589488e0ce2baa69ec1525a4e53e1b03f5a0d1
Skhoooler <dor15004@byui.edu>
2020-06-12 12:15:41 -0600

    Finished stubbing ShopFaceModel

--- Shopface/app/src/main/java/com/doria/byui/shopface/ShopFaceModel.java
+++ Shopface/app/src/main/java/com/doria/byui/shopface/ShopFaceModel.java
@@ -4,4 +4,23 @@ import java.util.Map;
 
 class ShopFaceModel {
     Map<String, Product> data;
+
+    ShopFaceModel(String query){
+
+    }
+
+    /**
+     * Returns a map full of Product objects
+     * @return a map with the raw data from the connect interface
+     */
+    public Map<String, Product> getAllData(){
+        return null;
+    }
+
+    /**
+     * Not sure what we were thinking with this one
+     */
+    public void search(){
+
+    }
 }
