e537f5ab3bfb60af13376510c82bcd4c552d5570
Skhoooler <dor15004@byui.edu>
2020-06-12 11:57:38 -0600

    Finished stubbing Product class

--- Shopface/app/src/main/java/com/doria/byui/shopface/Product.java
+++ Shopface/app/src/main/java/com/doria/byui/shopface/Product.java
@@ -1,4 +1,46 @@
 package com.doria.byui.shopface;
 
+import android.media.Image;
+
 class Product {
+    private String name;
+    private float price;
+    private Image pic;
+    private String desc;
+    private String link;
+    Product(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
 }
