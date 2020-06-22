b59973394a46ac85257a72588f8f2c1406dd718d
Skhoooler <dor15004@byui.edu>
2020-06-22 12:35:47 -0600

    Added the eBayConnect class, and created methods to construct the API call URL

---
+++ Shopface/app/src/main/java/com/doria/byui/shopface/EbayConnect.java
@@ -0,0 +1,49 @@
+package com.doria.byui.shopface;
+
+import com.ebay.services.finding.*;
+
+public class EbayConnect {
+
+    private String eBaySandboxAppID = "DavidDor-Shopface-SBX-7c8ee5576-a64d1c6f";
+    private String eBayAppID        = "DavidDor-Shopface-PRD-2c8e8a00c-c0119913";
+
+    EbayConnect(){
+
+    }
+
+    /**
+     * Constructs the correct API call URL to be able to send it off
+     * @return the URL to the API as a fully concatenated string
+     */
+    String constructAPICall(String opName)
+    {
+        String base          = "https://svcs.sandbox.ebay.com/services/search/FindingService/v1";
+        String operationName = "?OPERATION-NAME=";                                               // Required
+        String appName       = "&SECURITY-APPNAME=";                                             // Required
+        String dataFormat    = "&RESPONSE-DATA-FORMAT=JSON";                                     // Could also use XML instead of JSON
+
+
+        appName       = appName +eBayAppID;
+        operationName = operationName + opName;
+
+        return base + operationName + appName + dataFormat;
+    }
+
+    /**
+     * Constructs the correct API call URL to be able to send it off to the Sandbox
+     * @return the URL to the API as a fully concatenated string
+     */
+    String constructSandboxAPICall(String opName)
+    {
+        String base          = "https://svcs.sandbox.ebay.com/services/search/FindingService/v1";
+        String operationName = "?OPERATION-NAME=";                                               // Required
+        String appName       = "&SECURITY-APPNAME=";                                             // Required
+        String dataFormat    = "&RESPONSE-DATA-FORMAT=JSON";                                     // Could also use XML instead of JSON
+
+
+        appName       = appName + eBaySandboxAppID;
+        operationName = operationName + opName;
+
+        return base + operationName + appName + dataFormat;
+    }
+}
