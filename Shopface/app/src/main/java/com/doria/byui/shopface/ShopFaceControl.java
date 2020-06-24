b9da831e72f42fc17988e90641a1db7d8b2429ef
yugimumoto1 <sonnytoe96@gmail.com>
2020-06-24 12:17:24 -0600

    worked on shopface model and control as well as the display results UI

--- Shopface/app/src/main/java/com/doria/byui/shopface/ShopFaceControl.java
+++ Shopface/app/src/main/java/com/doria/byui/shopface/ShopFaceControl.java
@@ -5,8 +5,8 @@ import java.util.Map;
 public class ShopFaceControl {
 
     private Map<String, Product> rawData;
-    private Map<String, Product> sortedData;
-    private int index;
+    private Map<Integer, Product> sortedData;
+    private Integer index;
     public String query;
 
 
@@ -28,28 +28,29 @@ public class ShopFaceControl {
      * @return a map with the data that resulted from the search query
      */
     public Map<String, Product> getData(){
-        ShopFaceModel shopFaceModel = new ShopFaceModel(query);
-        rawData = shopFaceModel.getMapData(query);
 
-        return rawData;
+
+        return null;
     }
 
     /**
      * Overload of the default getData() method. This method returns the product at a
      * specific key
-     * @param key The key to access a specific product inside the map
+     * @param //key The key to access a specific product inside the map
      * @return returns a single product from the data map
      */
-    public Product getData(String key){
+    public Product getSortedData(){
+        Product searchedProduct = sortedData.get(index);
 
-        return null;
+        return searchedProduct;
     }
 
     /**
      * Gets the raw data from the ShopFaceModel class
      */
     private void getModelData(){
-
+        ShopFaceModel shopFaceModel = new ShopFaceModel(query);
+        rawData = shopFaceModel.getMapData(query);
     }
 
 }
