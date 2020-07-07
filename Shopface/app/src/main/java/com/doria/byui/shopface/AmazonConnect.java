c19a20ada53f77024e9e042d21913e6033509eb2
yugimumoto1 <sonnytoe96@gmail.com>
2020-07-07 11:50:15 -0600

    EbayConnect has been changed to remove errors but has not been tested

---
+++ Shopface/app/src/main/java/com/doria/byui/shopface/AmazonConnect.java
@@ -0,0 +1,11 @@
+package com.doria.byui.shopface;
+
+import java.util.Map;
+
+public class AmazonConnect implements ConnectStore
+{
+    @Override
+    public Map<Integer, Product> search(String query, boolean Sandbox) {
+        return null;
+    }
+}
