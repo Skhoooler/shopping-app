026693cb1eb863c2a32de42a78dc892bb9723d11
Skhoooler <dor15004@byui.edu>
2020-07-10 12:48:59 -0600

    Added Apache Math Statistics

--- Shopface/app/src/main/java/com/doria/byui/shopface/Connect.java
+++ Shopface/app/src/main/java/com/doria/byui/shopface/Connect.java
@@ -29,6 +29,13 @@ class Connect {
         allProducts.addAll(ebayResults);
         allProducts.addAll(amazonResults);
 
+        if (amazonResults == null){
+            System.out.println("Amazon Results is NULLLLLL >:(");
+
+        }
+        else{
+            System.out.println("Amazon results is not null :)");
+        }
         // Sorts via Price in descending order (cheapest first)
         Collections.sort(allProducts, new Comparator<Product>() {
             @Override
