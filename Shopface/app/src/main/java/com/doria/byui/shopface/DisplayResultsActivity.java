c618eab66cbac2c0789186aaf7bb0a0a37693163
Skhoooler <dor15004@byui.edu>
2020-07-15 12:25:16 -0600

    Added Code for DisplayResultsActivity Search

--- Shopface/app/src/main/java/com/doria/byui/shopface/DisplayResultsActivity.java
+++ Shopface/app/src/main/java/com/doria/byui/shopface/DisplayResultsActivity.java
@@ -2,8 +2,10 @@ package com.doria.byui.shopface;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 
@@ -22,8 +24,49 @@ public class DisplayResultsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_display_results);
         super.onPause();
     }
-    void onClickSearch(View view){
 
+    void onClickSearch(View view) throws InterruptedException {
+        EditText searchBox = (EditText)findViewById(R.id.editText);
+        final String query = searchBox.getText().toString();
+
+        final ArrayList[] amazonProducts = new ArrayList[]{new ArrayList<>()};
+        Thread amazonConnectThread = new Thread(new Runnable() {
+            @Override
+            public void run(){
+                //amazonProducts[0] = new AmazonConnect().search(query);
+            }
+        });
+
+        final ArrayList[] ebayProducts = new ArrayList[]{new ArrayList<>()};
+        Thread ebayConnectThread = new Thread(new Runnable() {
+            @Override
+            public void run(){
+                ebayProducts[0] = new EbayConnect().search(query);
+            }
+        });
+
+        // amazonConnectThread.start();
+        ebayConnectThread.start();
+
+        //amazonConnectThread.join();
+        ebayConnectThread.join();
+
+        Thread processProductsThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                ArrayList<Product> allProducts = new ArrayList<>();
+                allProducts.addAll(ebayProducts[0]);
+                //allProducts.addAll(amazonProducts[0]);
+
+                ArrayList<Product> sortedProducts = new ShopFaceControl(allProducts).sort(allProducts);
+                products = new ShopFaceView(sortedProducts).imageDecode();
+
+                Intent intent = new Intent(getBaseContext(), DisplayResultsActivity.class);
+                intent.putExtra("data", products);
+                startActivity(intent);
+            }});
+
+        processProductsThread.start();
     }
     void onClickBack(View view){
 
