f7d9e2b5aec2340cf2ef7a6bc8b21ad39f12dd6a
yugimumoto1 <sonnytoe96@gmail.com>
2020-07-15 12:23:31 -0600

    Shop face view partially worked on

--- Shopface/app/src/main/java/com/doria/byui/shopface/DisplayProductActivity.java
+++ Shopface/app/src/main/java/com/doria/byui/shopface/DisplayProductActivity.java
@@ -2,12 +2,24 @@ package com.doria.byui.shopface;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class DisplayProductActivity extends AppCompatActivity {
-    public ShopFaceView displayProducts(String query){
-        ShopFaceView view1 = null;
+    ArrayList<Product> sortedData;
+    public ShopFaceView displayProducts(ArrayList<Product> sortedProducts){
+        sortedData = sortedProducts;
+        int i = 0;
+        Context context = getApplicationContext();
+        ImageView my_image = new ImageView(context);
+        my_image.setImageDrawable(sortedProducts.get(i).getImage());
+        ImageView img= (ImageView) findViewById(R.id.image);
+        img.setImageResource(R.drawable.imageview1);
         return view1;
     }
 
